7e38e2a8810ac56f2bb8d917c9c9e5f841fb94eb
Aaron Alves <aaronalvesvs@gmail.com>
2024-02-21 00:02:34 -0300

    Configuração do useApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useApi } from "./hooks/useApi"
+
+type Repository = {
+  full_name: string
+  description: string
+}
+
 export function App() {
+  const {
+    data: repositories,
+    isFetching
+  } = useApi<Repository[]>('users/naanon/repos')
+
   return (
     <>
-      <h1>Hello Aaron</h1>
+      <ul>
+        {isFetching && <p>Carregando...</p>}
+        {repositories?.map((repository) => {
+          return (
+            <li key={repository.full_name}>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
